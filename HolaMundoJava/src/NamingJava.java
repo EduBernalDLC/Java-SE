1aa91d02101801c64d226bdc42d64919ed190303
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-01-28 16:58:53 -0600

    Convención de Nombres en Java #12

---
+++ HolaMundoJava/src/NamingJava.java
@@ -0,0 +1,31 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone= 33337777;
+        int celPhone= 77773333;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        System.out.println("");
+        String $countryName= "Spain";
+        String _backgroundColor= "Green";
+        System.out.println($countryName);
+        System.out.println(_backgroundColor);
+
+        System.out.println("");
+        String currency$= "MXN";
+        String background_color= "Blue";
+        System.out.println(currency$);
+        System.out.println(background_color);
+
+        //Las CONSTANTES se escriben en MAYÚSCULAS y contienen "_" si tiene dos palabras
+
+        int POSITION= -5;
+        int MAX_WIDTH= 9999;
+        int MIN_WIDTH= 1;
+
+        System.out.println("");
+        System.out.println(POSITION);
+        System.out.println(MAX_WIDTH);
+        System.out.println(MIN_WIDTH);
+    }//Fin main
+}//Fin clase
