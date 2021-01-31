48de9472f774f0941d862c504cb0f1d7cf114ca0
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-01-30 20:57:48 -0600

    Tipos de datos numéricos #14

---
+++ DataTypesJava/src/DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        int n= 1234567890;
+        long nLong= 12345678901L;
+
+        double nDouble= 123.4567890;
+        float nFloat= 123.0987654321F;
+
+    }//Fin main
+}//Fin clase
