8775a75a93545d33893845967fffe781d646d3bb
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-01-30 21:25:58 -0600

    char y boolean #15

--- DataTypesJava/src/DataTypes.java
+++ DataTypesJava/src/DataTypes.java
@@ -7,5 +7,24 @@ public class DataTypes {
         double nDouble= 123.4567890;
         float nFloat= 123.0987654321F;
 
+        //----------------------------Segunda clase---------------------------------------------
+        var salary= 10000;
+        var totalSalary= 0;
+
+        //La pensión es el 3% del salario
+        var pension= (salary * 0.03);
+
+        System.out.println("El salario es: " + salary);
+        System.out.println("La pensión es: " + pension);
+
+        totalSalary= (int) (salary - pension);
+        System.out.println("Salario total: " + totalSalary);
+
+        //------------Ahora con texto------------------------------
+        System.out.println("");
+
+        var employeeName= "Edu";
+        System.out.println("Empleado: " + employeeName + " con salario $" + totalSalary);
+
     }//Fin main
 }//Fin clase
