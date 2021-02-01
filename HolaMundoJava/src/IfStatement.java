1cc5b9726e215885ba6af5317c1ffccbb5f6d676
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-02-01 13:58:50 -0600

    Sentencia if #22

---
+++ HolaMundoJava/src/IfStatement.java
@@ -0,0 +1,12 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isBluetoothEnabled= true;
+        int fileSended= 3;
+
+        if(isBluetoothEnabled == true){
+            fileSended++;
+            System.out.println("Archivo enviado");
+        }//Fin if
+
+    }//Fin main
+}//Fin clase
