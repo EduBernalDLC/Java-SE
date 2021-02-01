6cacce956bba5dd74282e1a404eacfe19e7a4d6a
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-02-01 14:44:55 -0600

    Enlace de las variables y sentencia else #23

--- HolaMundoJava/src/IfStatement.java
+++ HolaMundoJava/src/IfStatement.java
@@ -1,12 +1,21 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnabled= true;
+        boolean isBluetoothEnabled= false;
         int fileSended= 3;
 
         if(isBluetoothEnabled == true){
+            int i= 0;
+            i++;
+
             fileSended++;
             System.out.println("Archivo enviado");
-        }//Fin if
+        }else{
+            System.out.println("Por favor enciende tu bluetooth");
+            fileSended--;
+        }//fin if else
+
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSended);
 
     }//Fin main
 }//Fin clase
